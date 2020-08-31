01ccb4b49fc04a8e4b7531b58d72b674b228b84b
somia <somiashahid63@gmail.com>
2020-08-31 16:21:47 +0500

    Assignment

---
+++ ImageCapture/app/src/main/java/com/example/imagecapture/MemoryContract.java
@@ -0,0 +1,16 @@
+package com.example.imagecapture;
+
+import android.provider.BaseColumns;
+
+class MemoryContract {
+
+
+    //An empty private constructor makes sure that the class is not going to be initialised.
+    private MemoryContract() {}
+
+    public static final class MemoryEntry implements BaseColumns {
+        public static final String TABLE_NAME = "memories";
+        public static final String COLUMN_TITLE = "title";
+        public static final String COLUMN_IMAGE = "image";
+    }
+}
---
+++ NoteApp/app/src/main/java/com/example/noteapp/MainActivity.java
@@ -0,0 +1,87 @@
+package com.example.noteapp;
+
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Adapter;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.Toolbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    Toolbar toolbar;
+    Adapter adapter;
+    TextView noItemText;
+    SimpleDatabase simpleDatabase;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        toolbar = findViewById(R.id.toolbar);
+        toolbar.setTitleTextColor(getResources().getColor(R.color.white));
+        setSupportActionBar(toolbar);
+        noItemText = findViewById(R.id.noItemText);
+        simpleDatabase = new SimpleDatabase(this);
+        List<ContactsContract.CommonDataKinds.Note> allNotes = simpleDatabase.getAllNotes();
+        recyclerView = findViewById(R.id.allNotesList);
+
+        if(allNotes.isEmpty()){
+            noItemText.setVisibility(View.VISIBLE);
+        }else {
+            noItemText.setVisibility(View.GONE);
+            displayList(allNotes);
+        }
+
+    }
+
+    private void displayList(List<ContactsContract.CommonDataKinds.Note> allNotes) {
+        RecyclerView recyclerView;
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        adapter = new Adapter(this,allNotes);
+        recyclerView.setAdapter(adapter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getItemId() == R.id.add){
+            Toast.makeText(this, "Add New Note", Toast.LENGTH_SHORT).show();
+            Intent i = new Intent(this,AddNote.class);
+            startActivity(i);
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        List<Note> getAllNotes = simpleDatabase.getAllNotes();
+        if(getAllNotes.isEmpty()){
+            noItemText.setVisibility(View.VISIBLE);
+        }else {
+            noItemText.setVisibility(View.GONE);
+            displayList(getAllNotes);
+        }
+
+
+    }
+
+}
